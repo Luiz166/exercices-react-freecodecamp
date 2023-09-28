34e843d3c968e5673c25c7bb8ecfe26ae9333207
Luiz <otavio.tata.morais@gmail.com>
2023-09-28 18:23:09 -0300

    rsrsrsrs

--- exercise7-custom-components/script.js
+++ exercise7-custom-components/script.js
@@ -91,7 +91,7 @@ const MainContent = () => {
         <div className="main">
             <h1>Reasons to learn React</h1>
             <ol className="mainContent">
-                <li>Pick up e-girls</li>
+                <li>Is very good</li>
                 <li>Get a job</li>
                 <li>Look's cool as fuck</li>
             </ol>
