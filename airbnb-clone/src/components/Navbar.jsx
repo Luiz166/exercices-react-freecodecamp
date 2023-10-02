db255823392fc69420029549b8f4d9e4049dee41
Luiz <otavio.tata.morais@gmail.com>
2023-10-02 15:41:41 -0300

    wow navbar muito complexa completado

---
+++ airbnb-clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import AirbnbLogo from "../images/airbnb-1.svg";
+
+export default function Navbar(){
+    return(
+        <nav className="Navbar">
+            <img src={AirbnbLogo} />
+        </nav>
+    )
+}
