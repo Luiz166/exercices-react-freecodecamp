d2caaf6b5e2ebc539605e6b89235a795a018d706
Luiz <otavio.tata.morais@gmail.com>
2023-10-04 22:43:57 -0300

    adicionando mais itens na data.js

--- airbnb-clone/src/data.js
+++ airbnb-clone/src/data.js
@@ -30,12 +30,77 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "https://i0.pickpik.com/photos/550/586/572/bicycling-riding-bike-riding-cyclists-preview.jpg",
+        coverImg: "https://images.unsplash.com/photo-1572111504021-40abd3479ddb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
         stats: {
             rating: 4.8,
             reviewCount: 2
         },
         location: "Norway",
         openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Swiss Alps Hiking Adventure",
+        description: "Explore the breathtaking Swiss Alps on foot and immerse yourself in nature's wonders. Our experienced guides will lead you to hidden gems and awe-inspiring vistas.",
+        price: 75,
+        coverImg: "https://images.unsplash.com/photo-1638300183456-ac362679cfba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+        stats: {
+            rating: 4.9,
+            reviewCount: 5
+        },
+        location: "Switzerland",
+        openSpots: 5
+    },
+    {
+        id: 5,
+        title: "New Zealand Kayaking Expedition",
+        description: "Paddle through crystal-clear waters, discover secluded coves, and witness the beauty of New Zealand's coastline on this unforgettable kayaking expedition.",
+        price: 60,
+        coverImg: "https://images.unsplash.com/photo-1500916434205-0c77489c6cf7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+        stats: {
+            rating: 4.7,
+            reviewCount: 3
+        },
+        location: "New Zealand",
+        openSpots: 4
+    },
+    {
+        id: 6,
+        title: "Kyoto Cultural Immersion",
+        description: "Experience the rich traditions of Kyoto, Japan. Visit ancient temples, participate in tea ceremonies, and stroll through historic streets in the heart of Japan's cultural capital.",
+        price: 80,
+        coverImg: "https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+        stats: {
+            rating: 4.9,
+            reviewCount: 7
+        },
+        location: "Kyoto, Japan",
+        openSpots: 2
+    },
+    {
+        id: 7,
+        title: "South African Safari Expedition",
+        description: "Embark on a thrilling safari adventure in the heart of South Africa's wilderness. Witness the Big Five and experience the magic of the African savanna.",
+        price: 120,
+        coverImg: "https://images.unsplash.com/photo-1498038116800-4159eb9b2a62?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+        stats: {
+            rating: 4.8,
+            reviewCount: 9
+        },
+        location: "South Africa",
+        openSpots: 6
+    },
+    {
+        id: 8,
+        title: "Bali Yoga Retreat",
+        description: "Reconnect with your inner self in the serene beauty of Bali. Join us for a rejuvenating yoga retreat surrounded by lush tropical landscapes and tranquil beaches.",
+        price: 90,
+        coverImg: "https://images.unsplash.com/photo-1559595500-e15296bdbb48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
+        stats: {
+            rating: 4.9,
+            reviewCount: 6
+        },
+        location: "Bali, Indonesia",
+        openSpots: 3
     }
 ]
