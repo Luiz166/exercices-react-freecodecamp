30663343a627a319b27e42071a329c76417861ae
Luiz <otavio.tata.morais@gmail.com>
2023-10-05 19:15:42 -0300

    adicionado a melhor cidade

--- projetos solo/travel-journal/src/data.js
+++ projetos solo/travel-journal/src/data.js
@@ -25,5 +25,14 @@ export default [
         summary: "Machu Picchu is a wonder of the world for a reason. Trekking the Inca Trail to this ancient citadel was a dream come true. Witnessed the sunrise over Machu Picchu, explored the Sacred Valley, and learned about the Inca civilization.",
         coverImage: "https://images.unsplash.com/photo-1587595431973-160d0d94add1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80",
         mapsLink: "https://maps.app.goo.gl/35cXzieu1QNJF5EU9"
-    }
+    },
+    {
+        id: 4,
+        date: "1 de outubro, 2023",
+        location: "Brasil",
+        title: "Curitiba",
+        summary: "É onde mora o amor da minha vida.",
+        coverImage: "https://images.unsplash.com/photo-1616642325314-fe17e194b380?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+        mapsLink: "https://maps.app.goo.gl/35cXzieu1QNJF5EU9"
+    },
 ]
