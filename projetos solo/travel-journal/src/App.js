4967c5769e2e789f7c614839a21c0efe48d0b23a
Luiz <otavio.tata.morais@gmail.com>
2023-10-05 02:18:01 -0300

    mapeando a data.js e renderizando os cards

--- projetos solo/travel-journal/src/App.js
+++ projetos solo/travel-journal/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import MainContent from "./components/MainContent";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <div className="container mx-auto">
+
+        <Card
+          key={item.id}
+          item={item}
+        />
+        <hr className="mt-5"/>
+      </div>
+    )
+  })
   return (
     <div className="App">
-      <Navbar/>
-      <MainContent/>
+      <Navbar />
+      {cards}
     </div>
   );
 }
