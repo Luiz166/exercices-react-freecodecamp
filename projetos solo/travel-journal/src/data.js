08e5bc86a7f9948b0bf3a3efc27bfdbb8cfd9849
Luiz <otavio.tata.morais@gmail.com>
2023-10-05 02:17:16 -0300

    modificando a array, pra mostrar melhor na pagina

--- projetos solo/travel-journal/src/data.js
+++ projetos solo/travel-journal/src/data.js
@@ -1,8 +1,9 @@
-export default Travel = [
+export default [
     {
         id: 1,
         date: "October 15, 2023",
-        location: "Santorini, Greece",
+        location: "Greece",
+        title: "Santorini",
         summary: "Santorini is a paradise on Earth. The breathtaking sunsets, charming white-washed buildings, and crystal-clear waters made this trip unforgettable. Explored Oia, hiked to the ancient city of Akrotiri, and relaxed on the stunning Red Beach.",
         coverImage: "https://images.unsplash.com/photo-1613395877344-13d4a8e0d49e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
         mapsLink: "https://maps.app.goo.gl/v2XXG8ouT2PEzTKk8"
@@ -10,7 +11,8 @@ export default Travel = [
     {
         id: 2,
         date: "June 25, 2023",
-        location: "Tokyo, Japan",
+        location: "Japan",
+        title: "Tokyo",
         summary:  "Tokyo is a city of contrasts. From the bustling streets of Shibuya to the tranquil gardens of Shinjuku Gyoen, this trip was a sensory overload. Ate sushi at Tsukiji Market, marveled at the cherry blossoms in Ueno Park, and immersed in Japanese culture at Asakusa.",
         coverImage: "https://images.unsplash.com/photo-1536768139911-e290a59011e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
         mapsLink: "https://maps.app.goo.gl/wue1aSFMD478irDY7"
@@ -18,7 +20,8 @@ export default Travel = [
     {
         id: 3,
         date: "November 8, 2023",
-        location: "Machu Picchu, Peru",
+        location: "Peru",
+        title: "Machu Picchu",
         summary: "Machu Picchu is a wonder of the world for a reason. Trekking the Inca Trail to this ancient citadel was a dream come true. Witnessed the sunrise over Machu Picchu, explored the Sacred Valley, and learned about the Inca civilization.",
         coverImage: "https://images.unsplash.com/photo-1587595431973-160d0d94add1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80",
         mapsLink: "https://maps.app.goo.gl/35cXzieu1QNJF5EU9"
