c5b4cce60eb74ef362346a05e769b7676d3ee138
Luiz <otavio.tata.morais@gmail.com>
2023-10-07 06:08:11 -0300

    renderizando imagem dos memes, muito poggers

--- projetos guiado/meme-generator/src/components/Form.js
+++ projetos guiado/meme-generator/src/components/Form.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import memesData from "../memesData";
 
 export default function Form() {
+    const [memeImage, setMemeImage] = useState("")
+
+    const FetchData = () => {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+    const handleClick = (e) => {
+        e.preventDefault()
+        FetchData()
+    }
+
     return (
         <div className="Main flex flex-col mt-10">
             <form className="form flex flex-col gap-10">
@@ -9,8 +22,9 @@ export default function Form() {
                     <input className="border border-gray-400 border-2 rounded-xl p-2 w-3/4" type="text" placeholder="And take my money!" />
                 </div>
                 <button className="text-2xl font-bold bg-gradient-to-r from-purple-900 to-purple-700 p-4 rounded-xl text-white"
-                    type="submit">Get a new meme image🖼</button>
+                    type="submit" onClick={handleClick}>Get a new meme image🖼</button>
             </form>
+            {memeImage && <img className="max-w-xl mt-5 m-auto" src={memeImage} alt="memeImage" />}
         </div>
     )
 }
